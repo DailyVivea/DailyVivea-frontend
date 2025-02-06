f16db95de79ca32740f3b1709601c61c81892c6f
inaemon <github32377@gmail.com>
2025-02-06 22:13:19 +0900

    feat: 캘린더에 필요한 데이터 정리

---
+++ src/components/Global/calendar.data.ts
@@ -0,0 +1,73 @@
+// 캘린더가 이곳 저곳에서 사용되면 key값 오류 발생 -> 해결법: 부모 컴포넌트명을 key로 사용
+export interface CalendarProps {
+    componentName: string
+}
+
+
+// 달력 헤더에 표시할 컬럼명
+export const calendarHeader = ['일', '월', '화', '수', '목', '금', '토'];
+
+
+// 달력에 표시할 날짜
+// 이번 달 날짜만 표시할 수도 있는데
+// 우리는 피그마 디자인상 지난 달, 다음 달 날짜까지 표시
+export const calendarDates = (date: Date) => {  // 기준으로 할 날짜(달력에서 선택된 날짜 등)를 받음
+    const dates = [];
+
+    // 이번 달 첫째 주
+    const firstDayOfMonth = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        1,
+    );
+    // 이번 달 마지막 주
+    const lastDayOfMonth = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0,
+    );
+    // 이전 달 마지막 주
+    const lastDayOfPrevMonth = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        0,
+    );
+
+
+    // [1] 이번 달 첫 주에 이전 달 마지막 날짜를 채우기
+    for (let i = firstDayOfMonth.getDay() - 1; i >= 0; i--) {
+        dates.push(
+        new Date(
+            date.getFullYear(),
+            date.getMonth() - 1,
+            lastDayOfPrevMonth.getDate() - i,
+        ),
+        );
+    }
+
+    // [2] 이번 달 날짜 채우기
+    for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
+        const currentYear = date.getFullYear();
+        const currentMonth = date.getMonth();
+        dates.push(new Date(currentYear, currentMonth, i));
+    }
+
+    // [3] 이번 달 마지막 주에 다음 달 첫 주 날짜를 채우기기
+    const totalCells = 42; // 7x6 그리드(일단 6주로 채우기)
+    const remainingCells = totalCells - dates.length;
+
+    for (let i = 1; i <= remainingCells; i++) {
+        const nextMonthDate = new Date(
+            date.getFullYear(),
+            date.getMonth() + 1,
+            i,
+        );
+
+        if (nextMonthDate.getDay() == 0)
+            break;
+
+        dates.push(nextMonthDate);
+    }
+
+    return dates;
+};
