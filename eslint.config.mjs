6b2ae011207b18e0967ee4f1d96d2eeee6515387
gzhan0226 <gzhan@naver.com>
2025-02-14 10:59:33 +0900

    eslint config 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // 사용되지 않는 변수 경고 끄기
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/prefer-as-const": "off",
+      "prefer-const": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
